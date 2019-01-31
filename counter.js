19a9017a7326eb7dd4b18372c79e225e5b6b7755
newtrout <trout0619@gmail.com>
2019-02-01 02:29:46 +0900

    テキストエリア下にカウント表示
    表示できたが、どうも1文字遅れになってしまう（必然的にエンター押す日本語なら大丈夫だが）

--- counter.js
+++ counter.js
@@ -1,6 +1,22 @@
+var insert_counter = false;
 window.addEventListener("keydown", function(){
     if (document.activeElement.tagName == "TEXTAREA"){
-        console.log(document.activeElement.value.length);
+        // console.log(document.activeElement.value.length);
+        String(ev = document.activeElement.value);
+        console.log(ev);
+        if (insert_counter){
+            document.getElementById("charactercounter").textContent = ev.length;
+        }
+        else{
+            document.activeElement.insertAdjacentHTML('afterend', '<p id="charactercounter">' + ev.length + '<p>');
+            insert_counter = true;
+            console.log(insert_counter);
+        }
+    }
+    else{
+        insert_counter = false;
+        console.log(insert_counter);
+
     }
 });
 
