bd3a5c128647c95bdc255aec21061d849f8bacfd
newtrout <trout0619@gmail.com>
2019-02-01 23:30:13 +0900

    カウントズレの解消、選択文字カウントの追加
    keydownをkeyup

--- counter.js
+++ counter.js
@@ -1,14 +1,15 @@
 var insert_counter = false;
-window.addEventListener("keydown", function(){
+window.addEventListener("keyup", function(){
     if (document.activeElement.tagName == "TEXTAREA"){
         // console.log(document.activeElement.value.length);
-        String(ev = document.activeElement.value);
-        console.log(ev);
+        ev_len = document.activeElement.value.replace(/\r?\n/g, '').length;
+        // console.log(ev);
         if (insert_counter){
-            document.getElementById("charactercounter").textContent = ev.length;
+            var selected_text_len = window.getSelection().toString().replace(/\r?\n/g, '').length;
+            document.getElementById("charactercounter").textContent = selected_text_len + " / " + ev_len;
         }
         else{
-            document.activeElement.insertAdjacentHTML('afterend', '<p id="charactercounter">' + ev.length + '<p>');
+            document.activeElement.insertAdjacentHTML('afterend', '<p id="charactercounter">' + ev_len + '<p>');
             insert_counter = true;
             console.log(insert_counter);
         }
@@ -19,5 +20,13 @@ window.addEventListener("keydown", function(){
 
     }
 });
+
+// window.addEventListener("blur", function () {
+//     if (insert_counter) {
+//         document.getElementById("charactercounter").remove();
+//         insert_counter = false;
+//         console.log("blur動いてるよ");
+//     }
+// });
 
 // テキストエリアがフォーカスされたとき、そのテキストエリアの文字を取得
